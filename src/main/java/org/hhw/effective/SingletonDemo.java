81f32c8379a436504bde678785ced5855442208b
vivi623 <vivi623@users.noreply.github.com>
2017-12-19 20:44:59 +0800

    单例模式

---
+++ src/main/java/org/hhw/effective/SingletonDemo.java
@@ -0,0 +1,42 @@
+package org.hhw.effective;
+
+/**
+ * 单例模式，注意java反射可以破坏单例
+ */
+public class SingletonDemo {
+    public static final SingletonDemo singleton = new SingletonDemo();
+
+    private SingletonDemo() {
+        //可以通过加强验证避免反射创建实例
+        if(singleton != null) {
+            throw new RuntimeException("instance error");
+        }
+    }
+
+    public static SingletonDemo getInstance() {
+        return singleton;
+    }
+
+    public static void main(String[] args) {
+        SingletonDemo demo1 = SingletonDemo.getInstance();
+        SingletonDemo demo2 = SingletonDemo.singleton;
+
+        System.out.println(demo1==demo2);
+
+        // 可以通过动态创建实例，创建多个实例
+        try {
+            Class clz= Class.forName("org.hhw.effective.SingletonDemo");
+            SingletonDemo demo3 = (SingletonDemo)clz.newInstance();
+            System.out.println(demo1==demo3);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
---
+++ src/main/java/org/hhw/effective/SingletonDemo2.java
@@ -0,0 +1,57 @@
+package org.hhw.effective;
+
+import java.io.*;
+
+/**
+ * 单例模式，序列化与序列化
+ */
+public class SingletonDemo2 implements Serializable {
+    public static final SingletonDemo2 singleton = new SingletonDemo2();
+
+    private SingletonDemo2() {
+        //可以通过加强验证避免反射创建实例
+        if(singleton != null) {
+            throw new RuntimeException("instance error");
+        }
+    }
+
+    /**
+     * 加上此行，可保证反实例化的对象和原对象是一个
+     * @return
+     */
+    private Object readResolve() {
+        return singleton;
+    }
+
+    public static SingletonDemo2 getInstance() {
+        return singleton;
+    }
+
+    public static void main(String[] args) {
+        try {
+            SingletonDemo2 singleton1 = SingletonDemo2.getInstance();
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File("E://demo1.txt")));
+            out.writeObject(singleton1);
+            out.close();
+
+            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(new File("E://demo1.txt")));
+            SingletonDemo2 singleton2 = (SingletonDemo2)inputStream.readObject();
+            inputStream.close();
+            System.out.println(singleton1 == singleton2);
+
+            ObjectInputStream inputStream2 = new ObjectInputStream(new FileInputStream(new File("E://demo1.txt")));
+            SingletonDemo2 singleton3 = (SingletonDemo2)inputStream2.readObject();
+            inputStream.close();
+
+            System.out.println(singleton3 == singleton2);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
