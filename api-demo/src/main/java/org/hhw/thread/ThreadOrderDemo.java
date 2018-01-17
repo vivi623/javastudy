00d1e6a683dcf1a90ad92ee78e668998bab62d16
vivi623 <vivi623@users.noreply.github.com>
2018-01-17 18:39:13 +0800

    线程执行顺序

--- api-demo/src/main/java/org/hhw/thread/ThreadOrderDemo.java
+++ api-demo/src/main/java/org/hhw/thread/ThreadOrderDemo.java
@@ -9,7 +9,6 @@ public class ThreadOrderDemo {
 
         System.out.println("main --- before");
 //        threadOrderJoin();
-
         threadOrderExcutor();
         System.out.println("main --- after");
     }
@@ -19,6 +18,7 @@ public class ThreadOrderDemo {
         //拒绝策略有4中，1. 直接抛出异常 2. 立即尝试执行超出队列的任务 3. 丢弃最新的任务  4. 丢弃最旧的数据
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         for(int i = 0; i < 100; i++) {
+            //excute submit 区别：一个有返回值一个没有，如果不需要返回值直接用excute
             executorService.execute(new ThreadDemo("线程" + i));
         }
         executorService.shutdown();
@@ -41,12 +41,7 @@ public class ThreadOrderDemo {
         };
         @Override
         public void run() {
-            try {
-                Thread.sleep(600000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-//            System.out.println(name + ":[" + Thread.currentThread().getName() + "]:正在执行");
+            System.out.println(name + ":[" + Thread.currentThread().getName() + "]:正在执行");
         }
     }
 
